f424ee844828ec60f6ab205bbe0d70c52d21acda
Dinushi Galagama Mudalige <z5118509@cse.unsw.edu.au>
2017-08-16 18:08:52 +1000

    excercise1 complete

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # COMP1521
+
+Lab03
+
+Bit-fields, Unions and Floats
+
+
+Objectives
+
+to gain experience with C union types
+to investigate how floating-point numbers are constructed
+
+
+Exercise #1: Investigate Bit-field Layout
+Exercise #2: Build Float Values using Bit-fields and Unions
--- lab03/where_are_the_bits.c
+++ lab03/where_are_the_bits.c
@@ -17,5 +17,28 @@ int main(void)
 
    printf("%ul\n",sizeof(x));
 
+   unsigned int *ptr;
+   x.a = 0;
+   x.b = 0;
+   x.c = 0;
+
+   printf("%d\n", x.a);
+   printf("%d\n", x.b);
+   printf("%d\n", x.c);
+
+   ptr = (unsigned int *)&x;
+   (*ptr)++; //may increment either x.a || x.c
+	     //depennding on which part is incremented
+	     //the value of x.?, when printed, will also change
+
+   printf("%d\n", x.a);
+   printf("%d\n", x.b);
+   printf("%d\n", x.c);
+             //using the value we can determine
+	     //the bit field alignment in memory
+	     //the answer that comes through is 1 0 0
+	     //so, x.a is changed
+	     //we know that when incremented the right most bit changes
+	     //so, the bit fiels is of the form x.c x.b x.a
    return 0;
 }
