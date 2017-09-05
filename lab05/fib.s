3e3caa7dc0d8537e37b71b1c73a70f428061a397
Dinushi Galagama Mudalige <Dinu@Dinushis-MacBook-Pro.local>
2017-09-06 08:47:48 +1000

    "basic coding"

--- lab05/fib.s
+++ lab05/fib.s
@@ -1,90 +1,134 @@
-# MIPS assembler to compute Fibonacci numbers
+    # MIPS assembler to compute Fibonacci numbers
 
-   .data
-msg1:
-   .asciiz "n = "
-msg2:
-   .asciiz "fib(n) = "
+    .data
+    msg1:
+    .asciiz "n = "
+    msg2:
+    .asciiz "fib(n) = "
 
-   .text
+    error: .asciiz "n must be > 0 "
 
-# int main(void)
-# {
-#    int n;
-#    printf("n = ");
-#    scanf("%d", &n);
-#    if (n >= 1)
-#       printf("fib(n) = %d\n", fib(n));
-#    else {
-#       printf("n must be > 0\n");
-#       exit(1);
-#    }
-#    return 0;
-# }
+    .text
 
-   .globl main
-main:
-   # prologue
-   addi $sp, $sp, -4
-   sw   $fp, ($sp)
-   move $fp, $sp
-   addi $sp, $sp, -4
-   sw   $ra, ($sp)
+    # int main(void)
+    # {
+    #    int n;
+    #    printf("n = ");
+    #    scanf("%d", &n);
+    #    if (n >= 1)
+    #       printf("fib(n) = %d\n", fib(n));
+    #    else {
+    #       printf("n must be > 0\n");
+    #       exit(1);
+    #    }
+    #    return 0;
+    # }
 
-   # function body
-   la   $a0, msg1       # printf("n = ");
-   li   $v0, 4
-   syscall
+    .globl main
+    main:
+    # prologue
+    addi $sp, $sp, -4
+    sw   $fp, ($sp)
+    move $fp, $sp
+    addi $sp, $sp, -4
+    sw   $ra, ($sp)
 
-   li   $v0, 5          # scanf("%d", &n);
-   syscall
-   move $a0, $v0
+    # function body
+    la   $a0, msg1       # printf("n = ");
+    li   $v0, 4
+    syscall
 
-   # ... add code to check (n >= 1)
-   # ... print an error message, if needed
-   # ... and return a suitable value from main()
+    li   $v0, 5          # scanf("%d", &n);
+    syscall
+    move $a0, $v0
 
-   jal  fib             # $s0 = fib(n);
-   nop
-   move $s0, $v0
+    li $t0, 1
+    bge $a0, $t0, error
+    # ... add code to check (n >= 1)
+    
+    error:
+        la $a0, error
+        li $v0, 4
+        syscall
 
-   la   $a0, msg2       # printf((fib(n) = ");
-   li   $v0, 4
-   syscall
+        li $a0, 0
+            
+    # ... print an error message, if needed
+    # ... and return a suitable value from main()
 
-   move $a0, $s0        # printf("%d", $s0);
-   li   $v0, 1
-   syscall
+    jal  fib             # $s0 = fib(n);
+    nop
+    move $s0, $v0
 
-   li   $a0, '\n'       # printf("\n");
-   li   $v0, 11
-   syscall
+    la   $a0, msg2       # printf((fib(n) = ");
+    li   $v0, 4
+    syscall
 
-   # epilogue
-   lw   $ra, ($sp)
-   addi $sp, $sp, 4
-   lw   $fp, ($sp)
-   addi $sp, $sp, 4
-   jr   $ra
+    move $a0, $s0        # printf("%d", $s0);
+    li   $v0, 1
+    syscall
+
+    li   $a0, '\n'       # printf("\n");
+    li   $v0, 11
+    syscall
+
+    # epilogue
+    lw   $ra, ($sp)
+    addi $sp, $sp, 4
+    lw   $fp, ($sp)
+    addi $sp, $sp, 4
+    jr   $ra
 
 
-# int fib(int n)
-# {
-#    if (n < 1)
-#       return 0;
-#    else if (n == 1)
-#       return 1;
-#    else
-#       return fib(n-1) + fib(n-2);
-# }
+    # int fib(int n)
+    # {
+    #    if (n < 1)
+    #       return 0;
+    #    else if (n == 1)
+    #       return 1;
+    #    else
+    #       return fib(n-1) + fib(n-2);
+    # }
 
-fib:
-   # prologue
-   # ... add a suitable prologue
+    fib:
 
-   # function body
-   move $v0, $0
+    # Compute and return fibonacci number
+    beqz $a0,zero   #if n=0 return 0
+    beq $a0,1,one   #if n=1 return 1
 
-   # epilogue
-   # ... add a suitable epilogue
-   jr   $ra
+    #Calling fib(n-1)
+    sub $sp,$sp,4   #storing return address on stack
+    sw $ra,0($sp)
+
+    sub $a0,$a0,1   #n-1
+    jal fib     #fib(n-1)
+    add $a0,$a0,1
+
+    lw $ra,0($sp)   #restoring return address from stack
+    add $sp,$sp,4
+    sub $sp,$sp,4   #Push return value to stack
+    sw $v0,0($sp)
+
+    #Calling fib(n-2)
+    sub $sp,$sp,4   #storing return address on stack
+    sw $ra,0($sp)
+
+    sub $a0,$a0,2   #n-2
+    jal fib     #fib(n-2)
+    add $a0,$a0,2
+
+    lw $ra,0($sp)   #restoring return address from stack
+    add $sp,$sp,4
+    lw $s7,0($sp)   #Pop return value from stack
+    add $sp,$sp,4
+
+    add $v0,$v0,$s7 # f(n - 2)+fib(n-1)
+    jr   $ra
+
+    zero:
+        li $v0,0
+        jr $ra
+
+    one:
+        li $v0,1
+        jr $ra
