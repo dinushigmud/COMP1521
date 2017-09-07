4809bd194c5510bdbf25d73bc5385ce3f569c099
Dinushi Galagama Mudalige <z5118509@cse.unsw.edu.au>
2017-09-07 14:24:21 +1000

    minor changes

--- ass1/life.s
+++ ass1/life.s
@@ -1,3 +1,22 @@
+# board.s ... Game of Life on a 10x10 grid
+
+   .data
+
+N: .word 10  # gives board dimensions
+
+board:
+   .byte 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+   .byte 1, 1, 0, 0, 0, 0, 0, 0, 0, 0
+   .byte 0, 0, 0, 1, 0, 0, 0, 0, 0, 0
+   .byte 0, 0, 1, 0, 1, 0, 0, 0, 0, 0
+   .byte 0, 0, 0, 0, 1, 0, 0, 0, 0, 0
+   .byte 0, 0, 0, 0, 1, 1, 1, 0, 0, 0
+   .byte 0, 0, 0, 1, 0, 0, 1, 0, 0, 0
+   .byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+   .byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+   .byte 0, 0, 1, 0, 0, 0, 0, 0, 0, 0
+
+newBoard: .space 100
 # prog.s ... Game of Life on a NxN grid
 #
 # Needs to be combined with board.s
