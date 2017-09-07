468e9d6e40dc96f63931e12018686027624cef2d
Dinushi Galagama Mudalige <Dinu@Dinushis-MacBook-Pro.local>
2017-09-07 15:58:44 +1000

    debug

--- ass1/prog.s
+++ ass1/prog.s
@@ -231,7 +231,7 @@ copyBackAndShow:
         ble $t6, $t7, loop_1_end
 
         loop_2:
-            ble $t8, $t6, loop_2_end
+            ble $t6, $t8, loop_2_end
 
             mul $t5, $t6, $t7
             add $t5, $t5, $t8
